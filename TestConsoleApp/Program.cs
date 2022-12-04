d1ddaf8f601d22ab1a72584d4f775609605e4534
StasCool <60066403+staTHICC@users.noreply.github.com>
2022-12-04 17:02:28 +0300

    a+b!
    e

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,6 +10,11 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+            Console.ReadLine();
         }
     }
 }
